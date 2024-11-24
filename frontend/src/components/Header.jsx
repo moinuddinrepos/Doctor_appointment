c4c258e1cebeff58c5e1d1d72c6d200f2ca75acd
moinuddinrepos <moincse786@gmail.com>
2024-11-24 22:02:46 +0530

    commited

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { assets } from '../assets/assets_frontend/assets'
 const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap bg-primary roun px-6 md:px-10 lg:px-20'>
+      <marquee behavior="" direction=""><span className='text-red-500 bg-white rounded-md p-2'>NOTE:</span> This project is under development. Working on backend process...</marquee>
         {/* -----------left side--------------- */}
         <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
             <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>Book Appointment <br /> With Trusted Doctors </p>
